0edc94259a418a746e422cadf5e88217e5e9342b
fidelis <fidelisnwabisi@gmail.com>
2023-07-18 18:30:12 +0100

    first commit

---
+++ lib/features/on_boarding/views/on_boarding_screen.dart
@@ -0,0 +1,19 @@
+// OnBoardingScreen
+// Image
+// Title
+// Skip
+// Swipe Inicator
+
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Container(),
+    );
+  }
+}
