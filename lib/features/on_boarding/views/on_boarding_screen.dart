b2a8f764d552fd1b15f131433f9481b9a57887af
fidelis <fidelisnwabisi@gmail.com>
2023-07-19 01:40:06 +0100

    added image and text to the on boarding screen

--- lib/features/on_boarding/views/on_boarding_screen.dart
+++ lib/features/on_boarding/views/on_boarding_screen.dart
@@ -13,7 +13,30 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(ImageRes.todo),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(ImageRes.todo),
+            Text(
+              "ToDo with Riverpod",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              "Welcome!!! DO you want to clear tasks super fast with ToDo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
