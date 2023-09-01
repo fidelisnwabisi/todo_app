4ab5a9a41d5f65f3902df77f341abaf7a5e206e5
fidelis <fidelisnwabisi@gmail.com>
2023-09-01 08:12:06 +0100

    added showSnackBar in core_uttils.dart added snackBar and page navigator push

--- lib/features/authentication/repository/authentication_repository.dart
+++ lib/features/authentication/repository/authentication_repository.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:pinput/pinput.dart';
+import 'package:todo_app/core/utils/core_utils.dart';
+import 'package:todo_app/features/authentication/views/otp_verification_screen.dart';
 
 class AuthenticationRepository {
   const AuthenticationRepository({
@@ -9,6 +12,7 @@ class AuthenticationRepository {
   final FirebaseAuth auth;
 
   Future<void> sendOTP({
+    required BuildContext context,
     required String phoneNumber,
   }) async {
     await auth.verifyPhoneNumber(
@@ -16,8 +20,20 @@ class AuthenticationRepository {
       verificationCompleted: (credential) {
         auth.signInWithCredential(credential);
       },
-      verificationFailed: (exception) {},
-      codeSent: (verificationId, _) {},
+      verificationFailed: (exception) {
+        CoreUtils.showSnackBar(
+          context: context,
+          message: "${exception.code}: ${exception.message}",
+        );
+      },
+      codeSent: (verificationId, _) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const OTPVerificationScreen(),
+          ),
+        );
+      },
       codeAutoRetrievalTimeout: (verificationId) {},
     );
   }
