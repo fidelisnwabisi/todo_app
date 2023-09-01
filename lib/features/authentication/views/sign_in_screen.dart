36c26549d5d10dc0442795e5720df9df4bf01d8b
fidelis <fidelisnwabisi@gmail.com>
2023-09-01 08:30:18 +0100

    added AuthenticationController

--- lib/features/authentication/views/sign_in_screen.dart
+++ lib/features/authentication/views/sign_in_screen.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/core/common/widgets/round_button.dart';
 import 'package:todo_app/core/common/widgets/white_space.dart';
 import 'package:todo_app/core/res/colours.dart';
 import 'package:todo_app/core/res/image_res.dart';
+import 'package:todo_app/features/authentication/controller/authentication_controller.dart';
+import 'package:todo_app/features/authentication/repository/authentication_repository.dart';
 import 'package:todo_app/features/authentication/views/otp_verification_screen.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -56,12 +59,11 @@ class SignInScreen extends StatelessWidget {
               RoundButton(
                 text: 'Send Code',
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const OTPVerificationScreen(),
+                  AuthenticationController(
+                    AuthenticationRepository(
+                      auth: FirebaseAuth.instance,
                     ),
-                  );
+                  ).sendOTP(context: context, phoneNumber: "phoneNumber");
                 },
               )
             ],
